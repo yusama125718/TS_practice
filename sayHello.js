1e30cfd1c4af2246bdf81c73872cf5ec17b29bc5
yusama125718 <98173828+yusama125718@users.noreply.github.com>
2025-02-09 10:25:25 +0900

    昨日〜今朝の勉強分

--- sayHello.js
+++ sayHello.js
@@ -12,9 +12,25 @@ function genNightGreet(firstname) {
 function sayGreet(firstname, greet) {
     console.log('${greet} ${firstname}');
 }
+// 引数にデフォルト値をつけれる
+function genGreet(firstname, greet) {
+    if (greet === void 0) { greet = 'Hello!'; }
+    console.log("${greet} ${firstname}");
+}
 // これならもちろん呼べる
 sayGreet('Katsuya', 'Good evening');
 // これでもいける
 sayGreet('Tanaka');
 sayHello(firstname);
 console.log(genNightGreet(firstname));
+// 関数を引数に取れる
+// 引数名: (関数の引数: 型) => 返り値の型
+// 以下の例ではstringの引数を1つ要求するstringの配列を返す関数を引数として要求している
+function singBird(bird) {
+    return bird('hato, kiji')[0] + "piyopiyo";
+}
+// こんな感じ
+function splitbird(birds) {
+    return birds.split(',');
+}
+console.log(singBird(splitbird));
--- sayHello.ts
+++ sayHello.ts
@@ -16,6 +16,11 @@ function sayGreet(firstname: string, greet?: string){
   console.log('${greet} ${firstname}')
 }
 
+// 引数にデフォルト値をつけれる
+function genGreet(firstname: string, greet: string = 'Hello!'){
+  console.log("${greet} ${firstname}")
+}
+
 // これならもちろん呼べる
 sayGreet('Katsuya', 'Good evening')
 // これでもいける
@@ -23,3 +28,17 @@ sayGreet('Tanaka')
 
 sayHello(firstname)
 console.log(genNightGreet(firstname))
+
+// 関数を引数に取れる
+// 引数名: (関数の引数: 型) => 返り値の型
+// 以下の例ではstringの引数を1つ要求するstringの配列を返す関数を引数として要求している
+function singBird(bird: (x: string) => string[]){
+  return bird('hato, kiji')[0] + "piyopiyo"
+}
+
+// こんな感じ
+function splitbird(birds: string): string[]{
+  return birds.split(',')
+}
+console.log(singBird(splitbird))
+
